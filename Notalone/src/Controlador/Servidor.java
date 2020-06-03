6bc269bfda816773cb05f2361671b413346e2d0d
feryv <feryv@LAPTOP-36SH2NU5>
2020-06-03 11:43:44 +0200

    03/06/2020

--- Notalone/src/Controlador/Servidor.java
+++ Notalone/src/Controlador/Servidor.java
@@ -1,40 +1,43 @@
 package Controlador;
 import javax.imageio.ImageIO;
+
+
+
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.*;
 
 public class Servidor
 {
-    public static void main(String[] args)
+	static int n=0;
+    public static void main(String[] args) throws IOException
     {
-        int port=2050;
-        try
-        {
-            ServerSocket serverSocket = new ServerSocket(port);
-
-            Socket clientSocket = serverSocket.accept();
-
-            InputStream inputStream = clientSocket.getInputStream();
-
-            System.out.println(inputStream);
-
-            byte[] imageAr = new byte[62100];
-            inputStream.read(imageAr);
-
-                BufferedImage image = ImageIO.read(new ByteArrayInputStream(imageAr));
-
-            System.out.println("Received " + image.getHeight() + "x" + image.getWidth() + ": " + System.currentTimeMillis());
-            ImageIO.write(image, "jpg", new File("C:\\UsuariosNotAlone\\Fernando\\imagenes\\penes.png"));
-
-            inputStream.close();
-            clientSocket.close();
-            serverSocket.close();
-        }
-        catch (UnknownHostException e){
-            System.out.println(e);
-        } catch (IOException e) {
-            System.out.println(e);
-        }
+    	
+    	ServerSocket servidor;		
+		servidor = new ServerSocket(2050);
+		System.out.println("Servidor iniciado...");
+		
+		while (true) {	
+			Socket cliente = new Socket();
+			cliente=servidor.accept();//esperando cliente	
+			HiloServidor hilo = new HiloServidor(cliente);
+			hilo.nombre("publi"+n);
+			n++;
+			hilo.start();
+			
+		}
+		}
     }
-}
+      
+        
+            
+
+            
+
+            
+
+            
+
+           
+       
+    
